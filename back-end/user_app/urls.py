ef0acb12e00100643432174e400125865c82753a
fravila08 <fr4v1l4@gmail.com>
2024-08-13 14:28:24 -0700

    create log in MVC

--- back-end/user_app/urls.py
+++ back-end/user_app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("register/", Register.as_view(), name="register user")]
+urlpatterns = [
+    path("register/", Register.as_view(), name="register user"),
+    path("login/", LogIn.as_view(), name="log in"),
+]
--- back-end/user_app/views.py
+++ back-end/user_app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status as s
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -14,8 +14,25 @@ class Register(APIView):
         if new_user.is_valid():
             user = new_user.save()
             token = Token.objects.create(user=user)
+            login(request, user)
             return Response(
                 {"user": user.username, "token": token.key}, status=s.HTTP_201_CREATED
             )
         else:
             return Response(new_user.errors, status=s.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        data = request.data.copy()
+        user = authenticate(
+            request, username=data.get("email"), password=data.get("password")
+        )
+        if user:
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response(
+                {"user": user.username, "token": token.key}, status=s.HTTP_200_OK
+            )
+        else:
+            return Response("No User Matching Credentials", status=s.HTTP_404_NOT_FOUND)
