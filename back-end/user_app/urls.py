ed831f8007ab13824075989b7912719cbd345706
fravila08 <fr4v1l4@gmail.com>
2024-08-13 15:21:18 -0700

    create user logout MVC

--- back-end/user_app/urls.py
+++ back-end/user_app/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("register/", Register.as_view(), name="register user"),
     path("login/", LogIn.as_view(), name="user login"),
+    path("logout/", LogOut.as_view(), name='user logout'),
 ]
--- back-end/user_app/views.py
+++ back-end/user_app/views.py
@@ -2,6 +2,8 @@ from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status as s
 from django.contrib.auth import authenticate, login, logout
 
@@ -34,3 +36,16 @@ class LogIn(APIView):
             )
         else:
             return Response("No User Matching Credentials", status=s.HTTP_404_NOT_FOUND)
+
+
+class UserView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class LogOut(UserView):
+    def post(self, request):
+        user = request.user
+        user.auth_token.delete()
+        logout(request)
+        return Response("You have successfully logged out", status=s.HTTP_200_OK)
